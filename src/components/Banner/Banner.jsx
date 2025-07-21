579c9b1ab048eaa86d8f0cccc17770f52afdb0bf
Pereverza Tetiana <tanyusha.vzletka82@gmail.com>
2025-07-21 16:30:30 +0300

    marking desctop style

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,15 +4,12 @@ import SearchBox from "../SearchBox/SearchBox";
 const Banner = () => {
   return (
     <section className={css.bannerSection}>
-      <div className={css.container }>
-        <div className={css.imageSearch}></div>
+      <div className={css.imageSearch}></div>
+      <div className={css.bannerInner}>
         <div className={css.boxTitle}>
           <h1 className={css.titleBanner}>
-            <span>Plan, Cook, and</span>
-            <br />
-            Share Your
-            <br />
-            Flavors
+            <span className={css.lineOne}>Plan, Cook, and </span>
+            <span className={css.lineTwo}> Share Your Flavors</span>
           </h1>
           <SearchBox />
         </div>

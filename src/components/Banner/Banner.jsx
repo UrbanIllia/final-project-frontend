8a346370164e849a9c6c3621cc6b372cf37c2ec6
Urban <urban.mutagen@gmail.com>
2025-07-22 22:02:06 +0300

    add register component

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,25 +1,48 @@
+// import css from "./Banner.module.css";
+// import SearchBox from "../SearchBox/SearchBox";
+
+// const Banner = () => {
+//   return (
+//     <section className={css.bannerSection}>
+//       <div className={css.container}>
+//         <img
+//           src="/images/Banner.png"
+//           alt="girl cooking"
+//           className={css.imageSearch}
+//         />
+//       </div>
+//       <div className={css.boxTitle}>
+//         <h1 className={css.titleBanner}>
+//           <span>Plan, Cook, and</span>
+//           <br />
+//           <span>Share Your</span>
+//           <br />
+//           <span>Flavors</span>
+//         </h1>
+//         <SearchBox />
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Banner;
 import css from "./Banner.module.css";
 import SearchBox from "../SearchBox/SearchBox";
 
 const Banner = () => {
   return (
     <section className={css.bannerSection}>
-      <div className={css.container}>
-        <img
-          src="/images/Banner.png"
-          alt="girl cooking"
-          className={css.imageSearch}
-        />
-      </div>
-      <div className={css.boxTitle}>
-        <h1 className={css.titleBanner}>
-          <span>Plan, Cook, and</span>
-          <br />
-          <span>Share Your</span>
-          <br />
-          <span>Flavors</span>
-        </h1>
-        <SearchBox />
+      <div className={css.contentContainer}>
+        <div className={css.boxTitle}>
+          <h1 className={css.titleBanner}>
+            <span>Plan, Cook, and</span>
+            <br />
+            <span>Share Your</span>
+            <br />
+            <span>Flavors</span>
+          </h1>
+          <SearchBox />
+        </div>
       </div>
     </section>
   );

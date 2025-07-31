489e21c3052174015284b26aacee4c362de37568
sh1xzyy <alekslungol@gmail.com>
2025-07-31 18:13:22 +0300

    code without excess comments

--- src/axiosConfig/Api.js
+++ src/axiosConfig/Api.js
@@ -1,62 +1,61 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const API = axios.create({
-  baseURL: "https://final-project-backend-rtvo.onrender.com/api/",
-  withCredentials: true,
-});
+	baseURL: 'https://final-project-backend-rtvo.onrender.com/api/',
+	withCredentials: true,
+})
 
-export const setAuthHeader = (accessToken) => {
-  API.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
-};
+export const setAuthHeader = accessToken => {
+	API.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
+}
 
 export const clearAuthHeader = () => {
-  API.defaults.headers.common["Authorization"] = "";
-};
+	API.defaults.headers.common['Authorization'] = ''
+}
 
 API.interceptors.request.use(
-  (config) => {
-    const accessToken = localStorage.getItem("accessToken");
-    if (accessToken) {
-      config.headers.Authorization = `Bearer ${accessToken}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+	config => {
+		const accessToken = localStorage.getItem('accessToken')
+		if (accessToken) {
+			config.headers.Authorization = `Bearer ${accessToken}`
+		}
+		return config
+	},
+	error => Promise.reject(error)
+)
 
 API.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    console.log("Interceptor caught error:", error.response?.status);
-    const originalRequest = error.config;
+	response => response,
+	async error => {
+		console.log('Interceptor caught error:', error.response?.status)
+		const originalRequest = error.config
 
-    const accessToken = localStorage.getItem("accessToken");
+		const accessToken = localStorage.getItem('accessToken')
 
-    if (
-      error.response?.status === 401 &&
-      !originalRequest._retry &&
-      !originalRequest.url.includes("/auth/refresh") &&
-      accessToken
-    ) {
-      originalRequest._retry = true;
-      try {
-        const response = await API.post("/auth/refresh");
+		if (
+			error.response?.status === 401 &&
+			!originalRequest._retry &&
+			!originalRequest.url.includes('/auth/refresh') &&
+			accessToken
+		) {
+			originalRequest._retry = true
+			try {
+				const response = await API.post('/auth/refresh')
 
-        const { accessToken: newAccessToken } = response.data;
+				const { accessToken: newAccessToken } = response.data
 
-        localStorage.setItem("accessToken", newAccessToken);
-        setAuthHeader(newAccessToken);
+				localStorage.setItem('accessToken', newAccessToken)
+				setAuthHeader(newAccessToken)
 
-        originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
-        return API(originalRequest);
-      } catch (refreshError) {
-        clearAuthHeader();
-        localStorage.removeItem("accessToken");
-        // window.location.href = "/login";
-        return Promise.reject(refreshError);
-      }
-    }
+				originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`
+				return API(originalRequest)
+			} catch (refreshError) {
+				clearAuthHeader()
+				localStorage.removeItem('accessToken')
+				return Promise.reject(refreshError)
+			}
+		}
 
-    return Promise.reject(error);
-  }
-);
+		return Promise.reject(error)
+	}
+)
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,63 +1,62 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import Logo from "../Logo/Logo";
-import BurgerMenu from "./BurgerMenu/BurgerMenu";
-import Navigation from "./Navigation/Navigation";
-import css from "./Header.module.css";
-import { logoutUserThunk } from "../../redux/operations/authOperations";
-// import { fetchUserThunk } from "../../redux/operations/userOperation";
-import { toast } from "react-toastify";
-import { selectAuthIsLoggedIn } from "../../redux/selectors/authSelector";
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import Logo from '../Logo/Logo'
+import BurgerMenu from './BurgerMenu/BurgerMenu'
+import Navigation from './Navigation/Navigation'
+import css from './Header.module.css'
+import { logoutUserThunk } from '../../redux/operations/authOperations'
+import { toast } from 'react-toastify'
+import { selectAuthIsLoggedIn } from '../../redux/selectors/authSelector'
 
 export default function Header() {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const isLoggedIn = useSelector(selectAuthIsLoggedIn);
-  const userName = useSelector((state) => state.user.user.name);
+	const [menuOpen, setMenuOpen] = useState(false)
+	const isLoggedIn = useSelector(selectAuthIsLoggedIn)
+	const userName = useSelector(state => state.user.user.name)
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+	const dispatch = useDispatch()
+	const navigate = useNavigate()
 
-  const handleLogout = async () => {
-    try {
-      await dispatch(logoutUserThunk()).unwrap();
-      toast.success("Logout successful!");
-    } catch (error) {
-      toast.error("Logout error: " + error);
-    } finally {
-      setMenuOpen(false);
-      navigate("/");
-    }
-  };
+	const handleLogout = async () => {
+		try {
+			await dispatch(logoutUserThunk()).unwrap()
+			toast.success('Logout successful!')
+		} catch (error) {
+			toast.error('Logout error: ' + error)
+		} finally {
+			setMenuOpen(false)
+			navigate('/')
+		}
+	}
 
-  return (
-    <header className={css.header}>
-      <div className={css.container}>
-        <Logo />
-        <BurgerMenu open={menuOpen} setOpen={setMenuOpen} />
+	return (
+		<header className={css.header}>
+			<div className={css.container}>
+				<Logo />
+				<BurgerMenu open={menuOpen} setOpen={setMenuOpen} />
 
-        <div className={css.desktopNav}>
-          <Navigation
-            isLoggedIn={isLoggedIn}
-            closeMenu={() => {}}
-            userName={userName}
-            onLogout={handleLogout}
-            isMobile={false}
-          />
-        </div>
-      </div>
+				<div className={css.desktopNav}>
+					<Navigation
+						isLoggedIn={isLoggedIn}
+						closeMenu={() => {}}
+						userName={userName}
+						onLogout={handleLogout}
+						isMobile={false}
+					/>
+				</div>
+			</div>
 
-      {menuOpen && (
-        <div className={`${css.mobileMenu} ${css.open}`}>
-          <Navigation
-            isLoggedIn={isLoggedIn}
-            closeMenu={() => setMenuOpen(false)}
-            userName={userName}
-            onLogout={handleLogout}
-            isMobile={true}
-          />
-        </div>
-      )}
-    </header>
-  );
+			{menuOpen && (
+				<div className={`${css.mobileMenu} ${css.open}`}>
+					<Navigation
+						isLoggedIn={isLoggedIn}
+						closeMenu={() => setMenuOpen(false)}
+						userName={userName}
+						onLogout={handleLogout}
+						isMobile={true}
+					/>
+				</div>
+			)}
+		</header>
+	)
 }
